598b788292b492c129a60a4510f1e2e9ac378c21
MD. Shimul <mdshemul480@gmail.com>
2022-01-26 18:27:28 +0600

    adding google map done

--- frontend/src/components/GoogleMap/Map/Map.jsx
+++ frontend/src/components/GoogleMap/Map/Map.jsx
@@ -4,6 +4,7 @@ const Map = ({ onClick, Children, ...options }) => {
   const mapRef = useRef(null);
   const [map, setMap] = useState(null);
 
+  // this will initialize the map
   useEffect(() => {
     if (mapRef.current && !map) {
       const map = new window.google.maps.Map(mapRef.current, {
@@ -13,10 +14,21 @@ const Map = ({ onClick, Children, ...options }) => {
     }
   }, [map, mapRef, options]);
 
+  // this is adding event listener to the map
+  useEffect(() => {
+    if (map && onClick) {
+      map.addListener('click', onClick);
+    }
+  }, [onClick, map]);
+
   return (
     <>
       <div ref={mapRef} id='map' />
-      {React.Children.map()}
+      {React.Children.map(Children, (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, { map });
+        }
+      })}
     </>
   );
 };
