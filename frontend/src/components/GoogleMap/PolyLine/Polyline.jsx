23c42c8d3b44fe609742d0bf03a70d4d3ff703d6
MD. Shimul <mdshemul480@gmail.com>
2022-01-26 23:31:26 +0600

    removing unused import

--- frontend/src/components/GoogleMap/PolyLine/Polyline.jsx
+++ frontend/src/components/GoogleMap/PolyLine/Polyline.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Polyline = ({ map, coordinates }) => {
   const [polyline, setPolyline] = useState();
